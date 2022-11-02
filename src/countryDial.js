144bcd1291e438e5beb62f197b07605ef7b4f266
Md  Anisur Rahman <anisurrahmanbup@gmail.com>
2022-11-02 10:52:34 +0600

    Added code to create Country Dial JS

--- src/countryDial.js
+++ src/countryDial.js
@@ -1,32 +1,3 @@
-import fs from "fs";
-import path from "path";
-
-function readData(err, data) {
-  if (eer) {
-    console.log(err);
-  }
-
-  let dataArray = data.split("icon:");
-  let newDataArray = [];
-
-  dataArray.map((d1) => {
-    if (d1.includes(".png")) {
-      let allSubStitutes = d1.split("/");
-      let filenameString = allSubStitutes[allSubStitutes.length - 1];
-      let finalFileNameArray = filenameString.split(".");
-      let finalFileName = finalFileNameArray[0];
-      let stringToPush = `name: '${finalFileName}', \n\t\t\t\t icon: ${d1}`;
-      newDataArray.push(stringToPush);
-    } else {
-      newDataArray.push(d1);
-    }
-  });
-  fs.writeFileSync(path.join(__dirname, "output.txt"), newDataArray.join(""));
-}
-
-let pathName = path.join(__dirname, "JournalDEV.txt");
-fs.readFile(pathName, "utf8", readData);
-
 let obj = {
   Afghanistan: { name: "Afghanistan", dial_code: "+93", code: "AF" },
   Albania: { name: "Albania", dial_code: "+355", code: "AL" },
